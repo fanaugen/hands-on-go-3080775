99437a1b986fec173c5ca74fa46d186588eb3006
Alex Jahraus <fanaugen@users.noreply.github.com>
2025-01-30 21:26:06 +0000

    Declare constants, quote a rune

--- types/constants/begin/main.go
+++ types/constants/begin/main.go
@@ -1,15 +1,24 @@
 // types/constants/begin/main.go
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"unicode"
+)
+
 // declare a constant representing pi
+const pi = 3.141593 // recognized by compiler as a float
 
 // declare a rune constant for the letter 'a'
+const a rune = 'A'
 
 func main() {
 	// print the value and types of pi and a
-	// fmt.Printf("pi: %v - %T\n", pi, pi)
-	// fmt.Printf("a: %c - %T\n", a, a)
+	// In the format string, %T is the data type
+	fmt.Printf("pi: %v is a %T\n", pi, pi)
+	fmt.Printf("a:  %c is a %T\n", a, a)
 
 	// use unicode package to confirm that the rune is a letter
-	// unicode.IsLetter(a)
+	fmt.Println(strconv.QuoteRune(a) + " is a letter? ", unicode.IsLetter(a))
 }
