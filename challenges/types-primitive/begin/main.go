917449835fc462d1e0b399822ab41ffe11a0add1
Alex Jahraus <fanaugen@users.noreply.github.com>
2025-01-30 22:08:20 +0000

    Challenge: global, local, and pointer

--- challenges/types-primitive/begin/main.go
+++ challenges/types-primitive/begin/main.go
@@ -1,18 +1,42 @@
 // challenges/types-primitive/begin/main.go
 package main
 
-// use type inference to create a package-level variable "val" and assign it a value of "global"
+import "fmt"
+
+// use type inference to make a package-level variable "val" and assign it a value of "global"
+var val = "global"
+
 
 func main() {
-	// create a local variable "val" and assign it an int value
+	val := 99 // local variable "val" 
 
-	// print the value of the local variable "val"
+	// print the value and the type of local variable "val"
+	fmtStr := "%v is a %T"
+	fmt.Printf("\nfunction-locally, " + fmtStr, val, val)
 
-	// print the value of the package-level variable "val"
+	// print the value and type of the package-level variable "val"
+	printPkgVal(fmtStr)
 
 	// update the package-level variable "val" and print it again
+  updatePkgVal("NEW VALUE")
+	printPkgVal(fmtStr)
 
 	// print the pointer address of the local variable "val"
+	ptr := &val
+	fmt.Printf("\nPointer: %v (%T)\n", ptr, ptr)
+  
+	// assign a value directly to the pointer address
+	// of the local variable "val"
+	// and print the value of the local variable "val"
+	*ptr = 1001
+  fmt.Println("local val is now", val)
+}
 
-	// assign a value directly to the pointer address of the local variable "val" and print the value of the local variable "val"
+func printPkgVal(fs string) {
+  fmt.Printf("\npagkage-globally, " + fs, val, val)
+}
+
+func updatePkgVal(new_value string) bool {
+	val = new_value
+	return true
 }
