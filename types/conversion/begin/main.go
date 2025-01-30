40bc6ff0395f3d10dbeace74bf193e15e6b1e43c
Alex Jahraus <fanaugen@users.noreply.github.com>
2025-01-30 21:30:58 +0000

    Type converstion and precision

--- types/conversion/begin/main.go
+++ types/conversion/begin/main.go
@@ -1,6 +1,11 @@
 // types/conversion/begin/main.go
 package main
 
+import "fmt"
+
 func main() {
-	// declare float and convert it to an unsigned int
+  f := 42.00001   // float
+	i := uint(f)    // unsigned int ❗Precision lost❗
+
+	fmt.Printf("%v is a %T, but %v is a %T", f, f, i, i)
 }
