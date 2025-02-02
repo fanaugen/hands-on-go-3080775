3d11e6b0412c89707c06d9013ed0185002055050
Alex Jahraus <fanaugen@users.noreply.github.com>
2025-02-02 17:57:33 +0000

    Rename functions

--- challenges/types-primitive/begin/main.go
+++ challenges/types-primitive/begin/main.go
@@ -8,35 +8,35 @@ var val = "global"
 
 
 func main() {
-	val := 99 // local variable "val" 
+	val := 99 // local variable "val" is an int 
 
 	// print the value and the type of local variable "val"
-	fmtStr := "%v is a %T"
+	fmtStr := "val is %v <%T>"
 	fmt.Printf("\nfunction-locally, " + fmtStr, val, val)
 
 	// print the value and type of the package-level variable "val"
-	printPkgVal(fmtStr)
+	printGlobal(fmtStr)
 
 	// update the package-level variable "val" and print it again
-  updatePkgVal("NEW VALUE")
-	printPkgVal(fmtStr)
+  updateGlobal("NEW VALUE")
+	printGlobal(fmtStr)
 
 	// print the pointer address of the local variable "val"
 	ptr := &val
-	fmt.Printf("\nPointer: %v (%T)\n", ptr, ptr)
+	fmt.Printf("\nPointer: %v <%T>\n", ptr, ptr)
   
 	// assign a value directly to the pointer address
 	// of the local variable "val"
 	// and print the value of the local variable "val"
 	*ptr = 1001
-  fmt.Println("local val is now", val)
+  fmt.Println("\nlocal val is now", val)
 }
 
-func printPkgVal(fs string) {
+func printGlobal(fs string) {
   fmt.Printf("\npagkage-globally, " + fs, val, val)
 }
 
-func updatePkgVal(new_value string) bool {
+func updateGlobal(new_value string) bool {
 	val = new_value
 	return true
 }
