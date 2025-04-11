fe735dc29024970c01da98d251d2afe4c27106bc
Alex Jahraus <fanaugen@users.noreply.github.com>
2025-04-11 16:11:16 +0000

    Slices, make, append

--- types/slices/begin/main.go
+++ types/slices/begin/main.go
@@ -1,18 +1,23 @@
 // types/slices/begin/main.go
 package main
 
+import "fmt"
+
 func main() {
-	// declare a slice string the make function
-	//
+	// declare a slice of strings using the make built-in function
+	names := make([]string, 0) // initial length is zero, not the same as capacity
 
 	// append 3 names to the slice
-	//
+	names = append(names, "Alice")
+	names = append(names, "Bob")
+	names = append(names, "Claire")
 
-	// print the slice
+	fmt.Println(names)
+	fmt.Printf("%#v\n", names)
 
 	// slice the slice using syntax slice[low:high]
-	// [Jane Mary]
-	// [Jane Mary]
-	// [John]
-	// [John Jane Mary]
+	fmt.Println(names[1:3]) // [Bob Claire]
+	fmt.Println(names[1:])  // [Bob Claire]
+	fmt.Println(names[:1])  // [Alice]
+	fmt.Println(names[:])   // [Alice Bob Claire]
 }
