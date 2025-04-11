c79c2a98b839e4b6ed018885df73548e4f7c08d7
Alex Jahraus <fanaugen@users.noreply.github.com>
2025-04-11 15:53:14 +0000

    Arrays

--- types/arrays/begin/main.go
+++ types/arrays/begin/main.go
@@ -1,13 +1,19 @@
 // types/arrays/begin/main.go
 package main
 
+import "fmt"
+
 func main() {
-	// declare an array of integers
+	var a [3]int // array of size 3, data type integer
 
-	// print the array
+	fmt.Println(a)
 
-	// set the first element to 1
+	a[0] = 1
 
-	// print the array
-
+	fmt.Println(a)
 }
+
+// ⚠️ NOTE: an Array in Go cannot be resized.
+//   For convenience, in practice we mostly work with slices.
+//   A slice is like a sliding window onto an underlying array,
+//   and it hides sizing concerns, as it’ll automatically resize.
