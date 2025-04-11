a424d7c0d6b66444847c56e14d29301d7942ed62
Alex Jahraus <fanaugen@users.noreply.github.com>
2025-04-11 14:19:52 +0000

    simple struct in Go

--- types/structs/fields/begin/main.go
+++ types/structs/fields/begin/main.go
@@ -1,13 +1,20 @@
 // types/structs/fields/begin/main.go
 package main
 
+import "fmt"
+
 // define a struct type for author
-//
+type author struct {
+	first, last string
+}
 
 func main() {
 	// intialize author
-	//
+	king := author{first: "Stephen", last: "King"}
 
 	// print the author
-	// fmt.Printf("%#v\n", a)
+	fmt.Printf("%v\n", king)
+	// print the internal representation of the struct
+	fmt.Printf("%#v\n", king) // %#v is like `inspect` in ruby
+	fmt.Printf("The type of local variable `king` is: %T\n", king)
 }
