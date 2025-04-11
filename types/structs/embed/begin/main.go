a9fcef78287e40858d2ed86b6e976087de072ade
Alex Jahraus <fanaugen@users.noreply.github.com>
2025-04-11 15:41:31 +0000

    Embedded struct

--- types/structs/embed/begin/main.go
+++ types/structs/embed/begin/main.go
@@ -4,29 +4,43 @@ package main
 import "fmt"
 
 type person struct {
-	first string
-	last  string
+	first, last string
 }
 
-// fullName returns the full name of a person
+// `fullName` returns the full name of a person
 func (p person) fullName() string {
 	return p.first + " " + p.last
 }
 
-// define author and embed person
-//
+/*******************************************************************
+  Go does not support inheritance, but it does support composition.
+	This means that we can embed one struct inside another struct.
+********************************************************************/
+// embed person
+type author struct {
+	person         // ← embed a person type here, which is also a struct
+	penName string // ← pen name is an "attribute" (field) specific to the author type
+}
 
-// override fullName method for author
-//
-
+/*
+ * (i) NOTE:
+ * You can also embed an interface type inside a struct.
+ * This will be explored in the chapter on interfaces.
+ */
 func main() {
 	// initialize and print a person's full name
-	p := person{
-		first: "Toni",
-		last:  "Morrison",
-	}
+	p := person{first: "Stephen", last: "King"}
 	fmt.Println(p.fullName())
 
-	// initialize and print an author's full name
-	//
+	// initialize and print an author with an embedded person
+	a := author{
+		person:  p,
+		penName: "Richard Bachman",
+	}
+	fmt.Println(a.fullName())
+}
+
+// override fullName method for author
+func (a author) fullName() string {
+	return fmt.Sprintf("%s (writing as %s)", a.person.fullName(), a.penName)
 }
