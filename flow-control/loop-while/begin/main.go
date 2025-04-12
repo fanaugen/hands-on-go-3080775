c2d0fe29cfc2b64a81746f183cc69ea4f5ce9530
Alexander Jahraus <fanaugen@gmail.com>
2025-04-12 19:46:52 +0200

    While loop: just the condition

--- flow-control/loop-while/begin/main.go
+++ flow-control/loop-while/begin/main.go
@@ -1,10 +1,15 @@
 // flow-control/loop-while/begin/main.go
 package main
 
+import "fmt"
+
 func main() {
 	// initialize a variable to check if the loop should continue
-	//
+	i := 5
 
 	// iterate while the condition is true
-	//
+	for i > 0 {
+		fmt.Println(i)
+		i--
+	}
 }
