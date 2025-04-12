7665edb41a7a462b3e51aca797af4256da175e77
Alexander Jahraus <fanaugen@gmail.com>
2025-04-12 19:42:57 +0200

    for loop with int index

--- flow-control/loop-basic/begin/main.go
+++ flow-control/loop-basic/begin/main.go
@@ -1,10 +1,14 @@
 // flow-control/loop-basic/begin/main.go
 package main
 
+import "fmt"
+
 func main() {
 	// declare a string to iterate over
-	//
+	str := "apple"
 
 	// iterate over the string with basic for loop
-	//
+	for i := 0; i < len(str); i++ {
+		fmt.Printf("Char %d: %s\n", i, string(str[i]))
+	}
 }
