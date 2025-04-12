ff9540aabd2bae56ec50fca699da20b59a944e52
Alexander Jahraus <fanaugen@gmail.com>
2025-04-12 20:02:21 +0200

    infinite loop

--- flow-control/loop-infinite/main.go
+++ flow-control/loop-infinite/main.go
@@ -1,7 +1,18 @@
 // flow-control/loop-infinite/main.go
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
-	for {
+
+	i := 0
+	for { // a loop without any condition is an endless loop
+		// this will keep repeating, using 100% CPU until interrupted
+		i++
+		fmt.Println(i)
+		time.Sleep(time.Second)
 	}
 }
