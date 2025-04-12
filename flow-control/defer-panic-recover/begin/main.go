8555f68492fb1e4eb7e98c4169ec62d0ebac2f46
Alexander Jahraus <fanaugen@gmail.com>
2025-04-12 20:27:25 +0200

    defer, panic, recover()

--- flow-control/defer-panic-recover/begin/main.go
+++ flow-control/defer-panic-recover/begin/main.go
@@ -3,14 +3,22 @@ package main
 
 import "fmt"
 
-func cleanup(msg string) {
-	fmt.Println(msg)
-}
+func cleanup(msg string) { fmt.Println("INFO: cleaning up " + msg) }
 
 func main() {
-	// defer function calls
+	// defer function calls. These will be called at the end, but last-in-first-out
+	defer cleanup("first deferred")
+	defer cleanup("second deferred")
 
-	// defer recovery
+	fmt.Println("INFO: doing work in main function...")
+
+	// defer recovery. recover() is a Go built-in function
+	defer func() { // IIFE (Immediately Invoked Function Expression), aka (anonymous) inline function
+		if err := recover(); err != nil {
+			fmt.Println("WARN: recovered from panic", err)
+		}
+	}()
 
 	// panic
+	panic("ERROR: uh-oh")
 }
